af4fe9923da257bebba2125ef8e6ece64a466d52
paramzwork <ramel.panis@ricklee.com.ph>
2026-07-17 11:05:15 +0800

    style: update header and sidebar components for improved aesthetics; add select component

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -4,7 +4,7 @@ import { Search, Bell, MessageSquare } from "lucide-react";
 
 export default function HeaderComponent() {
   return (
-    <header className="bg-white border-b border-gray-100 px-8 py-4 font-lexend">
+    <header className="bg-slate-700 border-b border-gray-100 px-8 py-4 font-lexend">
       <div className="flex items-center justify-between">
         {/* LEFT SIDE */}
         <div className="flex items-center gap-4">
@@ -20,17 +20,17 @@ export default function HeaderComponent() {
 
           {/* Notifications */}
           <button className="relative p-2 hover:bg-gray-100 rounded-lg transition-colors cursor-pointer">
-            <Bell className="w-5 h-5 text-gray-500" />
+            <Bell className="w-5 h-5 text-white" />
             <span className="absolute top-1 right-1 w-2 h-2 bg-red-500 rounded-full"></span>
           </button>
 
           {/* Messages */}
           <button className="relative p-2 hover:bg-gray-100 rounded-lg transition-colors">
-            <MessageSquare className="w-5 h-5 text-gray-500" />
+            <MessageSquare className="w-5 h-5 text-white" />
             <span className="absolute top-1 right-1 w-2 h-2 bg-primary-500 rounded-full"></span>
           </button>
         </div>
-        <div className="text-sm text-gray-500">
+        <div className="text-sm text-white">
           {new Date().toLocaleDateString("en-US", {
             weekday: "long",
             year: "numeric",
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['10.68.0.212'],
+  allowedDevOrigins: ["10.68.0.212"],
 };
 
 export default nextConfig;
