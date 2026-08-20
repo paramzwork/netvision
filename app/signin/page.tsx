f180bb2a72fb7d7de5b80dc6f991c2360c0d3bcf
paramzwork <ramel.panis@ricklee.com.ph>
2026-08-20 13:04:55 +0800

    update page handler name

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,7 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { redirect } from "next/navigation";
 import SignInComponent from "./SignInComponent";
 
-export default async function SignUpPage() {
+export default async function SignInPage() {
   const userCount = await prisma.users.count();
 
   if (userCount === 0) {
