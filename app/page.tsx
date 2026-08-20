2ef852834f9b78bed6788ab09cc0d1746759909b
paramzwork <ramel.panis@ricklee.com.ph>
2026-08-20 17:04:48 +0800

    feat: add dynamic export to Home and SignUpPage components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import { redirect } from "next/navigation";
-
+export const dynamic = "force-dynamic";
 export default async function Home() {
   const userCount = await prisma.users.count();
 
